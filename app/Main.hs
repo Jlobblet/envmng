19aaa2f8f7de1ddcf8f616e1f27560ce83c4c8b3
Jlobblet <jlobblet-github@gmx.co.uk>
2022-03-26 13:08:29 +0000

    Remove some unused imports

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,11 @@
 module Main where
 
 import Change (runChanges)
-import GHC.IO.Exception (ExitCode (ExitSuccess))
+import GHC.IO.Exception (ExitCode)
 import Options.Applicative (execParser)
 import Opts (handleOpts)
 import Parsers (opts, parseChanges)
 import System.Environment (getArgs)
-import System.Exit (exitWith)
 
 main :: IO ExitCode
 main = getArgs >>= either (const parseOpts) runChanges . parseChanges
